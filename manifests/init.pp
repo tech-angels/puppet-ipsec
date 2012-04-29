7b5dfd45fbcef7433f79f4cb1c5e467e4ebf5cbe
Gilbert Roulot <github@issarlk.net>
2012-04-29 21:26:32 +0200

    Creates a directory for SSL certificates
    Creates a directory with links to Puppet SSL files, plus
    a link to CA with a hashed filename.

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,11 +37,33 @@ class ipsec::base {
     content => template("ipsec/racoon.conf.erb")
   }
 
+  file { 'racoon-ssl-dir':
+    name   => '/etc/racoon/ssl',
+    ensure => directory,
+  }
+
+  file { 'racoon-ssl-certificate':
+    name   => "/etc/racoon/ssl/${fqdn}.cert",
+    ensure => link,
+    target => "/var/lib/puppet/ssl/certs/${fqdn}.pem",
+  }
+
+  file { 'racoon-ssl-key':
+    name   => "/etc/racoon/ssl/${fqdn}.key",
+    ensure => link,
+    target => "/var/lib/puppet/ssl/private_keys/${fqdn}.pem",
+  }
+
+  exec { 'hash-puppet-ca':
+    command => 'ln -s /var/lib/puppet/ssl/certs/ca.pem /etc/racoon/ssl/`openssl x509 -hash -noout -in /var/lib/puppet/ssl/certs/ca.pem`.0',
+    unless  => 'test -L /etc/racoon/ssl/`openssl x509 -hash -noout -in /var/lib/puppet/ssl/certs/ca.pem`.0',
+  }
+
   Ip_connection <<| sourceip == "$ipaddress" |>>  # we can only use one filter expression, 'and' or 'or' are not supported (yet (version 25.1)).
 
   service { "racoon":
     ensure  => running,
-    require => [Package["ipsec-tools"], File["setkey.conf", "racoon.conf"]],
+    require => [Package["ipsec-tools"], File["setkey.conf", "racoon.conf", "racoon-ssl-dir", "racoon-ssl-certificate", "racoon-ssl-key"], Exec['hash-puppet-ca']],
     stop    => "export SETKEY_FLUSH_OPTIONS=-FP && /etc/init.d/racoon stop"
   }
 }
